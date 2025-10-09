b5bee4eb62c2a68e216e8560e4eac11fa824270e
msor-1 <42msor@gmail.com>
2025-10-08 19:40:36 -0700

    Add files via upload

---
+++ Script_prompts.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+
+set /p "name=What is your name? "
+
+echo Nice to meet you, %name%.
+
+echo.
+
+set /p "age=How old are you? "
+
+echo %age%? A wonderful age to explore the world.
+
+echo.
+pause
